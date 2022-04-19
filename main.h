2abad1fbb71363ca21e8899db6b68197b72a55b1
Peacebern <peacebern@gmail.com>
2022-04-19 09:24:42 -0700

    [feat]: created a function to format specifiers

--- main.h
+++ main.h
@@ -22,5 +22,11 @@ typedef struct convert conver_t;
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int unsigned_integer(va_list);
 
 #endif
