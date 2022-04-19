94f7185bea342363608489b89c966e1ef8734f19
Peacebern <peacebern@gmail.com>
2022-04-19 07:38:24 -0700

    [feat]: main.h file

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+* struct op - flag / function object
+* @c: flag
+* @f: function
+*/
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list);
+} op_t;
+
+int _printf(const char *format, ...);
